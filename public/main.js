1d2a63f1d69336f4e11d9b228f1ec0465bff9a4a
Rachel Simonsen <rachellsimonsen@gmail.com>
2022-04-13 10:52:00 -0600

    toying with divs

--- public/main.js
+++ public/main.js
@@ -1,1 +1,11 @@
-const cardField = document.querySelector('#cardField')
+const cardField = document.querySelector('cardField')
+const imageUrl = document.getElementById('addPhoto')
+const caption = document.getElementById('addCaption')
+const form = document.getElementById('input')
+
+
+const baseURL = `http://localhost:4024/api/capstone`
+
+const errCallback = err => console.log(err.response.data)
+
+console.log(caption)
